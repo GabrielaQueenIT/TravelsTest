8336eaf1aa5c3ce4cc4b6b1d9d52d43a7145014f
Gabrysia <gabadel93@gmail.com>
2023-03-16 16:50:43 +0100

    Add new test - SignUpTest

---
+++ src/test/java/SignUpTest.java
@@ -0,0 +1,45 @@
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class SignUpTest {
+
+    @Test
+    public void signUp() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        String lastName = "Testowy";
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+        driver.findElement(By.name("firstname")).sendKeys("Bartek");
+        driver.findElement(By.name("lastname")).sendKeys("Testowy");
+        driver.findElement(By.name("phone")).sendKeys("111111111");
+        driver.findElement(By.name("email")).sendKeys("tester2@testerw.com");
+        driver.findElement(By.name("password")).sendKeys("Test123");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Test123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+
+//        WebElement h3 = driver.findElement(By.xpath("//h3[text()='Hi, Bartek Testowy']"));
+        String  h3 = driver.findElement(By.xpath("//h3[@class='RTL']")).getText();
+
+        Assert.assertTrue(h3.contains(lastName));
+        Assert.assertFalse(false, h3);
+        Assert.assertEquals(h3, "Hi, Bartek Testowy");
+
+    }
+}
